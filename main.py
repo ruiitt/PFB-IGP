b6018d514197758f0234c959ef1822f183e37977
toasterbotz <141663502+toasterbotz@users.noreply.github.com>
2023-08-10 19:48:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
-import overhead
-import cash_on_hand
-import profit_and_loss
+# Import paths to main.py
 
-def main():
-    # Execute functions and collect their results
-    highest_overhead, max_percentage = overhead.find_highest_overhead(overhead.csv_file_path)
-    cash_deficits = cash_on_hand.cash_on_hand(cash_on_hand.csv_file_path)
-    profit_deficits = profit_and_loss.find_profit_deficits(profit_and_loss.csv_file_path)
+from pathlib import Path
+from cash_on_hand import cash_on_hand
+from overhead import find_highest_overhead
+from profit_and_loss import find_profit_deficits
 
-    # Create main.txt and write the results
-    with open("main.txt", "w") as output_file:
-        output_file.write("[HIGHEST OVERHEAD] " + highest_overhead.upper() + f": {max_percentage:.2f}%\n\n")
+# Define the paths to the input CSV files
+cash_csv_path = Path(".github/workflows/cash-on-hand-usd.csv")
+overhead_csv_path = Path(".github/workflows/overheads.csv")
+profit_csv_path = Path(".github/workflows/profit-and-loss-usd.csv")
 
-        output_file.write("[CASH DEFICITS]\n")
-        for result in cash_deficits:
-            output_file.write(result + "\n")
-        output_file.write("\n")
+# Execute functions from different modules
+cash_deficits = cash_on_hand(cash_csv_path)
+highest_overhead, max_percentage = find_highest_overhead(overhead_csv_path)
+profit_deficits = find_profit_deficits(profit_csv_path)
 
-        output_file.write("[PROFIT DEFICITS]\n")
-        for day, amount in profit_deficits:
-            output_file.write(f"[PROFIT DEFICIT] DAY: {day}, AMOUNT: USD{amount:.2f}\n")
+# Write results to summary_report.txt
+with open("summary_report.txt", "w") as summary_file:
 
-if __name__ == "__main__":
-    main()
+    summary_file.write(f"[HIGHEST OVERHEAD] {highest_overhead.upper()}: {max_percentage:.2f}%\n")
 
-    # Read and print the content of main.txt
-    with open("main.txt", "r") as output_file:
-        print(output_file.read())
+    for result in cash_deficits:
+        summary_file.write(result + "\n")
+
+    for day, amount in profit_deficits:
+        summary_file.write(f"[PROFIT DEFICIT] DAY: {day}, AMOUNT: USD{amount:.2f}\n")
+
+# Print the contents of the summary_report.txt file
+with open("summary_report.txt", "r") as summary_file:
+    print(summary_file.read())
